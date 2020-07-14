0d96d6762c678b641bc9cddb25e3ab85b286721f
wzj <wzj@eead9d37-86bc-cd46-9d00-32314a84245c>
2020-07-14 02:20:14 +0000

    git-svn-id: https://39.108.144.9:8443/svn/yiduoli_web@4 eead9d37-86bc-cd46-9d00-32314a84245c

--- public/themes/admin/views/home.blade.php
+++ public/themes/admin/views/home.blade.php
@@ -1,41 +1,136 @@
 <div class="main">
-    <div class="main_full">
+    <div class="main_full" style="margin-top: 15px;">
         <div class="layui-col-md12">
-            <div class="layui-card">
-                <!-- <div class="layui-card-header">待办事项</div> -->
-                <div class="layui-card-body">
-
-                    <div class="fb-carousel fb-backlog " lay-anim="" lay-indicator="inside" lay-arrow="none" >
-                        <div carousel-item="">
-                            <ul class="layui-row fb-clearfix ">
-                                <li class="layui-col-xs3">
-                                    @permission(home())
-                                    <a lay-href="" class="fb-backlog-body">
-                                        <h3>会员量</h3>
-                                        <p><cite>66</cite></p>
-                                    </a>
-                                    @endpermission
-                                </li>
-                                <li class="layui-col-xs3">
-                                    <a lay-href="" class="fb-backlog-body">
-                                        <h3>待审帖子</h3>
-                                        <p><cite>12</cite></p>
-                                    </a>
-                                </li>
-                                <li class="layui-col-xs3">
-                                    <a lay-href="" class="fb-backlog-body">
-                                        <h3>待审商品</h3>
-                                        <p><cite>99</cite></p>
-                                    </a>
-                                </li>
-                                <li class="layui-col-xs3">
-                                    <a lay-href="" class="fb-backlog-body">
-                                        <h3>待发货</h3>
-                                        <p><cite>20</cite></p>
-                                    </a>
-                                </li>
-                            </ul>
+            <div class="layui-card-box layui-col-space15  fb-clearfix">
+                <div class="layui-col-sm6 layui-col-md3">
+                    <div class="layui-card">
+                        <div class="layui-card-header">
+                            <b>今日销售额</b>
+                            <label>(昨日$)</label>
+                            <span class="layui-badge layui-bg-blue layuiadmin-badge">日</span>
                         </div>
+                        <div class="layui-card-body layuiadmin-card-list">
+                            <p class="layuiadmin-big-font">$1<span class="c2">(2)</span></p>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="layui-col-sm6 layui-col-md3">
+                    <div class="layui-card">
+                        <div class="layui-card-header">
+                            <b>总销售额</b>
+                            <span class="layui-badge layui-bg-blue layuiadmin-badge">总</span>
+                        </div>
+                        <div class="layui-card-body layuiadmin-card-list">
+                            <p class="layuiadmin-big-font">$1</p>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="layui-col-sm6 layui-col-md3">
+                    <div class="layui-card">
+                        <div class="layui-card-header">
+                            <b>今日订单量</b>
+                            <label>(昨日1)</label>
+                            <span class="layui-badge layui-bg-blue layuiadmin-badge">日</span>
+                        </div>
+                        <div class="layui-card-body layuiadmin-card-list">
+                            <p class="layuiadmin-big-font">1<span class="c2">(2)</span></p>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="layui-col-sm6 layui-col-md3">
+                    <div class="layui-card">
+                        <div class="layui-card-header">
+                            <b>总订单量</b>
+                            <span class="layui-badge layui-bg-blue layuiadmin-badge">总</span>
+                        </div>
+                        <div class="layui-card-body layuiadmin-card-list">
+                            <p class="layuiadmin-big-font">2</p>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="layui-col-sm6 layui-col-md3">
+                    <div class="layui-card">
+                        <div class="layui-card-header">
+                            客户数
+                            <span class="layui-badge layui-bg-blue layuiadmin-badge">总</span>
+                        </div>
+                        <div class="layui-card-body layuiadmin-card-list">
+                            <p class="layuiadmin-big-font">2</p>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="layui-col-sm6 layui-col-md3">
+                    <div class="layui-card">
+                        <div class="layui-card-header">
+                            收集客户数
+                            <span class="layui-badge layui-bg-blue layuiadmin-badge">总</span>
+                        </div>
+                        <div class="layui-card-body layuiadmin-card-list">
+                            <p class="layuiadmin-big-font">2</p>
+
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="layui-col-sm6 layui-col-md3">
+                    <div class="layui-card">
+                        <div class="layui-card-header">
+                            产品数
+                            <span class="layui-badge layui-bg-blue layuiadmin-badge">总</span>
+                        </div>
+                        <div class="layui-card-body layuiadmin-card-list">
+                            <p class="layuiadmin-big-font">2</p>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="layui-col-sm6 layui-col-md3">
+                    <div class="layui-card">
+                        <div class="layui-card-header">
+                            已发营销邮件
+                            <span class="layui-badge layui-bg-blue layuiadmin-badge">总</span>
+                        </div>
+                        <div class="layui-card-body layuiadmin-card-list">
+                            <p class="layuiadmin-big-font">2</p>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="power-box  fb-clearfix">
+                <p>常用功能</p>
+                <div class="power-box-con">
+                    <div class="power-box-item layui-col-md2">
+                        <a href="{{ guard_url('order/create') }}">
+                            添加订单
+                        </a>
+                    </div>
+                    <div class="power-box-item layui-col-md2">
+                        <a href="{{ guard_url('customer/create') }}">
+                            添加客户
+                        </a>
+                    </div>
+                    <div class="power-box-item layui-col-md2">
+                        <a href="{{ guard_url('new_customer/create') }}">
+                            添加收集客户
+                        </a>
+                    </div>
+                    <div class="power-box-item layui-col-md2">
+                        <a href="{{ guard_url('goods/create') }}">
+                            添加产品
+                        </a>
+                    </div>
+                    <div class="power-box-item layui-col-md2">
+                        <a href="#">
+                            营销邮箱
+                        </a>
                     </div>
                 </div>
             </div>
