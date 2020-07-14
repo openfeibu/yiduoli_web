063894e46af218fb7b3e1af9640e51b18903a9ff
wzj <wzj@eead9d37-86bc-cd46-9d00-32314a84245c>
2020-07-14 02:01:19 +0000

    git-svn-id: https://39.108.144.9:8443/svn/yiduoli_web@2 eead9d37-86bc-cd46-9d00-32314a84245c

--- public/themes/admin/views/auth/login.blade.php
+++ public/themes/admin/views/auth/login.blade.php
@@ -20,19 +20,28 @@
 
 
 <div class="login layui-anim layui-anim-up">
+	
 	<div class="login-con">
+		<div class="login-left">
+			<p>To Be One of Word-leading Biotech Enterprises</p>
+			<span>成为世界领先的生物技术企业</span>
+		</div>
+		<div class="login-right">
         {!! Theme::partial('message') !!}
 		<div class="login-con-title">
-			<img src="/images/logo.png"/>
-			<p>FBCMS</p>
+			
+			
 		</div>
 		{!!Form::vertical_open()->id('login')->method('POST')->class('layui-form')->action(guard_url('login')) !!}
 
 		<input name="email" placeholder="邮箱"  type="text" lay-verify="required" class="layui-input" >
 		<input name="password" lay-verify="required" placeholder="密码"  type="password" class="layui-input">
-
-		<input value="登录" lay-submit lay-filter="login" style="width:100%;" type="submit" class="login_btn">
+		<div class="login_btn-box">
+			<input value="登录" lay-submit lay-filter="login" style="width:100%;" type="submit" class="login_btn">
+		</div>
+		
 		<input id="rememberme" type="hidden" name="rememberme" value="1">
 		{!!Form::Close()!!}
+		</div>
 	</div>
 </div>
