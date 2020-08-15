96cb05daba4a51805f81c4651ff15575098c263f
wzj <wzj@eead9d37-86bc-cd46-9d00-32314a84245c>
2020-08-15 09:04:41 +0000

    git-svn-id: https://39.108.144.9:8443/svn/yiduoli_web@120 eead9d37-86bc-cd46-9d00-32314a84245c

--- app/Repositories/Presenter/NavListPresenter.php
+++ app/Repositories/Presenter/NavListPresenter.php
@@ -5,6 +5,8 @@ namespace App\Repositories\Presenter;
 use App\Models\Nav;
 use App\Models\NavCategory;
 use App\Repositories\Eloquent\NavRepository;
+use App\Repositories\Eloquent\ProductCategoryRepository;
+use App\Repositories\Eloquent\ProductRepository;
 use App\Repositories\Presenter\FractalPresenter;
 use Route;
 
@@ -39,66 +41,7 @@ class NavListPresenter extends FractalPresenter
         $category_id = NavCategory::where('slug',$slug)->value('id');
         $navs = $this->navRepository->navs($category_id);
 
-        $html = '';
-        if($navs) {
-
-            foreach ($navs as $nav) {
-
-                if(!$nav->is_menu)
-                {
-                    continue;
-                }
-                if(($nav->slug !== '#') && !Route::has($nav->slug)) {
-                    continue;
-                }
-
-                $class = '';
-
-                if($nav->active) {
-                    $class .= 'active';
-                }
-
-                $html .= '<li class="'.$class.'">';
-                $href = ($nav->slug == '#') || (!Route::has($nav->slug)) ? 'javascript:;' : $nav->url;
-                $html .= sprintf('<a href="%s">%s %s</a><div class="line transition "></div>', $href, $nav->icon_html, $nav->name);
-
-                if(!isset($nav->sub)) {
-                    $html .= '</li>';
-                    continue;
-                }
-
-                $i = 0;
-
-                foreach ($nav->sub as $sub) {
-                    if(!$sub->is_menu)
-                    {
-                        continue;
-                    }
-
-                    if(($sub->slug !== '#') && !Route::has($sub->slug)) {
-                        continue;
-                    }
-                    if($i == 0)
-                    {
-                        $html .= '<dl>';
-                    }
-                    $href = ($sub->slug == '#') || ($sub->slug == '')? 'javascript:;' : $sub->url;
-                    $icon = $sub->icon_html ? $sub->icon_html : '';
-
-                    $class = $sub->active ? 'layui-this' : '' ;
-
-                    $html .= sprintf('<dd class="'.$class.'"><a href="%s">%s %s</a></dd>', $href, $icon, $sub->name);
-                    $i++;
-                }
-                if($i)
-                {
-                    $html .= '</dl>';
-                }
-
-
-                $html .= '</li>';
-            }
-        }
+        $html = $this->getNavHtml($navs);
 
         return $html;
     }
@@ -110,52 +53,38 @@ class NavListPresenter extends FractalPresenter
         $html = '';
         if($navs) {
 
-            foreach ($navs as $nav) {
+        foreach ($navs as $nav) {
 
-                if(!$nav->is_menu)
+            if(!$nav->is_menu)
+            {
+                continue;
+            }
+            if(($nav->slug !== '#') && !Route::has($nav->slug)) {
+                continue;
+            }
+
+            $class = '';
+
+            if($nav->active) {
+                $class .= 'active';
+            }
+
+            $html .= '<li class="'.$class.'">';
+            $href = ($nav->slug == '#') || (!Route::has($nav->slug)) ? 'javascript:;' : $nav->url;
+            $html .= sprintf('<a href="%s">%s %s</a>', $href, $nav->icon_html, $nav->name);
+
+            $i = 0;
+            if($nav->slug == 'pc.product.index')
+            {
+                $product_categories = app(ProductCategoryRepository::class)->getListCategories(0);
+                foreach ($product_categories as $key => $category)
                 {
-                    continue;
-                }
-                if(($nav->slug !== '#') && !Route::has($nav->slug)) {
-                    continue;
-                }
-
-                $class = '';
-
-                if($nav->active) {
-                    $class .= 'active';
-                }
-
-                $html .= '<li class="'.$class.'">';
-                $href = ($nav->slug == '#') || (!Route::has($nav->slug)) ? 'javascript:;' : $nav->url;
-                $html .= sprintf('<a href="%s">%s %s</a>', $href, $nav->icon_html, $nav->name);
-
-                if(!isset($nav->sub)) {
-                    $html .= '</li>';
-                    continue;
-                }
-
-                $i = 0;
-
-                foreach ($nav->sub as $sub) {
-                    if(!$sub->is_menu)
-                    {
-                        continue;
-                    }
-
-                    if(($sub->slug !== '#') && !Route::has($sub->slug)) {
-                        continue;
-                    }
                     if($i == 0)
                     {
                         $html .= '<span class="caret "></span><dl>';
                     }
-                    $href = ($sub->slug == '#') || ($sub->slug == '')? 'javascript:;' : $sub->url;
-                    $icon = $sub->icon_html ? $sub->icon_html : '';
 
-                    $class = $sub->active ? 'layui-this' : '' ;
-
-                    $html .= sprintf('<dd class="'.$class.'"><a href="%s">%s %s</a></dd>', $href, $icon, $sub->name);
+                    $html .= sprintf('<dd class=""><a href="%s">%s %s</a></dd>', route('pc.product.index',['product_category_id' => $category->id]), '', $category->name);
                     $i++;
                 }
                 if($i)
@@ -165,8 +94,44 @@ class NavListPresenter extends FractalPresenter
 
 
                 $html .= '</li>';
+                continue;
             }
+
+            if(!isset($nav->sub)) {
+                $html .= '</li>';
+                continue;
+            }
+
+            foreach ($nav->sub as $sub) {
+                if(!$sub->is_menu)
+                {
+                    continue;
+                }
+
+                if(($sub->slug !== '#') && !Route::has($sub->slug)) {
+                    continue;
+                }
+                if($i == 0)
+                {
+                    $html .= '<span class="caret "></span><dl>';
+                }
+                $href = ($sub->slug == '#') || ($sub->slug == '')? 'javascript:;' : $sub->url;
+                $icon = $sub->icon_html ? $sub->icon_html : '';
+
+                $class = $sub->active ? 'layui-this' : '' ;
+
+                $html .= sprintf('<dd class="'.$class.'"><a href="%s">%s %s</a></dd>', $href, $icon, $sub->name);
+                $i++;
+            }
+            if($i)
+            {
+                $html .= '</dl>';
+            }
+
+
+            $html .= '</li>';
         }
+    }
 
         return $html;
     }
@@ -197,13 +162,34 @@ class NavListPresenter extends FractalPresenter
                 $html .= ' <div class="footer-link">';
                 $href = ($nav->slug == '#') || (!Route::has($nav->slug)) ? 'javascript:;' : $nav->url;
                 $html .= sprintf('<div class="footer-link-t"><a href="%s">%s %s</a></div>', $href, $nav->icon_html, $nav->name);
+                $i = 0;
+                if($nav->slug == 'pc.product.index')
+                {
+                    $product_categories = app(ProductCategoryRepository::class)->getListCategories(0);
+                    foreach ($product_categories as $key => $category)
+                    {
+                        if($i == 0)
+                        {
+                            $html .= '<ul>';
+                        }
+
+                        $html .= sprintf('<li class=""><a href="%s">%s %s</a></li>', route('pc.product.index',['product_category_id' => $category->id]), '', $category->name);
+                        $i++;
+                    }
+                    if($i)
+                    {
+                        $html .= '</ul>';
+                    }
+
+
+                    $html .= '</div>';
+                    continue;
+                }
 
                 if(!isset($nav->sub)) {
                     $html .= '</div>';
                     continue;
                 }
-
-                $i = 0;
 
                 foreach ($nav->sub as $sub) {
                     if(!$sub->is_menu)
@@ -236,6 +222,92 @@ class NavListPresenter extends FractalPresenter
             }
         }
 
+        return $html;
+    }
+    public function getNavHtml($navs,$caret='')
+    {
+        $html = '';
+        if(!$navs) {
+            return $html;
+        }
+        foreach ($navs as $nav) {
+
+            if(!$nav->is_menu)
+            {
+                continue;
+            }
+            if(($nav->slug !== '#') && !Route::has($nav->slug)) {
+                continue;
+            }
+
+            $class = '';
+
+            if($nav->active) {
+                $class .= 'active';
+            }
+
+            $html .= '<li class="'.$class.'">';
+            $href = ($nav->slug == '#') || (!Route::has($nav->slug)) ? 'javascript:;' : $nav->url;
+            $html .= sprintf('<a href="%s">%s %s</a><div class="line transition "></div>', $href, $nav->icon_html, $nav->name);
+
+            $i = 0;
+            if($nav->slug == 'pc.product.index')
+            {
+                $product_categories = app(ProductCategoryRepository::class)->getListCategories(0);
+                foreach ($product_categories as $key => $category)
+                {
+                    if($i == 0)
+                    {
+                        $html .= '<dl>';
+                    }
+
+                    $html .= sprintf('<dd class=""><a href="%s">%s %s</a></dd>', route('pc.product.index',['product_category_id' => $category->id]), '', $category->name);
+                    $i++;
+                }
+                if($i)
+                {
+                    $html .= '</dl>';
+                }
+
+
+                $html .= '</li>';
+                continue;
+            }
+            if(!isset($nav->sub)) {
+                $html .= '</li>';
+                continue;
+            }
+
+            foreach ($nav->sub as $sub) {
+                if(!$sub->is_menu)
+                {
+                    continue;
+                }
+
+                if(($sub->slug !== '#') && !Route::has($sub->slug)) {
+                    continue;
+                }
+                if($i == 0)
+                {
+                    $html .= $caret.'<dl>';
+                }
+
+                $href = ($sub->slug == '#') || ($sub->slug == '')? 'javascript:;' : $sub->url;
+                $icon = $sub->icon_html ? $sub->icon_html : '';
+
+                $class = $sub->active ? 'layui-this' : '' ;
+
+                $html .= sprintf('<dd class="'.$class.'"><a href="%s">%s %s</a></dd>', $href, $icon, $sub->name);
+                $i++;
+            }
+            if($i)
+            {
+                $html .= '</dl>';
+            }
+
+
+            $html .= '</li>';
+        }
         return $html;
     }
 }
