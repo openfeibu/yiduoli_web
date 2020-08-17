61a32a78495ee00f322aa08472a236ab8ff0611b
wzj <wzj@eead9d37-86bc-cd46-9d00-32314a84245c>
2020-08-17 13:50:47 +0000

    git-svn-id: https://39.108.144.9:8443/svn/yiduoli_web@138 eead9d37-86bc-cd46-9d00-32314a84245c

--- public/themes/pc/partials/header.blade.php
+++ public/themes/pc/partials/header.blade.php
@@ -1,11 +1,11 @@
 <header class="navbar-fixed-top  fadeInUp animated transition500" data-wow-duration=".6s" data-wow-delay=".3s">
     <div class="headerBg transition500">
         <div class="container w1400">
-            <div class="logo pull-left fadeInUp animated transition500">
+            <div class="logo newLogo pull-left fadeInUp animated transition500">
                 <a href="{{ route('pc.home') }}">
                     <h1 hidden="">{{ setting('station_name') }}</h1>
-                    <img class="logo1" src="{{ '/image/original'.setting('logo') }}" alt="{{ setting('station_name') }}" class="block">
-                    <img class="logo2" src="{{ '/image/original'.setting('logo') }}" alt="{{ setting('station_name') }}" class="block">
+                    <img class="logo1 " src="{{ '/image/original'.setting('logo') }}" alt="{{ setting('station_name') }}" class="block">
+                    <img class="logo2 " src="{{ '/image/original'.setting('logo') }}" alt="{{ setting('station_name') }}" class="block">
                 </a>
             </div>
             <div class="headerRight pull-right transition500">
