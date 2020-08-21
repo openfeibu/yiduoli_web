9238c04785747d878ff84ec0004aded338e0cd3e
wzj <wzj@eead9d37-86bc-cd46-9d00-32314a84245c>
2020-08-21 02:47:29 +0000

    git-svn-id: https://39.108.144.9:8443/svn/yiduoli_web@154 eead9d37-86bc-cd46-9d00-32314a84245c

--- public/themes/pc/config.php
+++ public/themes/pc/config.php
@@ -60,7 +60,7 @@ return [
         'beforeRenderTheme'  => function ($theme) {
 
             $theme->asset()->usePath()->add('bootstrap_css', 'css/bootstrap.min.css');
-            $theme->asset()->usePath()->add('fb-main_css', 'css/fb-main.css');
+            $theme->asset()->usePath()->add('fb-main_css', 'css/fb-main.css?v=2020082018');
             $theme->asset()->usePath()->add('animate_css', 'css/animate.min.css');
             $theme->asset()->add('swiper_css', 'lib/swiper/swiper-3.4.2.min.css');
 
