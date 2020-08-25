43ee8f7b0cc5c1d5b061f1f5a08a631954df5e5b
wzj <wzj@eead9d37-86bc-cd46-9d00-32314a84245c>
2020-08-25 12:24:50 +0000

    git-svn-id: https://39.108.144.9:8443/svn/yiduoli_web@170 eead9d37-86bc-cd46-9d00-32314a84245c

--- app/Models/ProductCategory.php
+++ app/Models/ProductCategory.php
@@ -16,4 +16,21 @@ class ProductCategory extends BaseModel
     protected $config = 'model.product.product_category';
 
     public $timestamps = false;
+
+    public function getParentNamesAttribute()
+    {
+        $category_ids = $this->attributes['category_ids'];
+        if(!$category_ids)
+        {
+            return "最顶级";
+        }
+        $category_id_arr = explode(',',$category_ids);
+        $parent_name_arr = [];
+        foreach ($category_id_arr as $key => $category_id)
+        {
+            $name = self::where('id',$category_id)->value('name');
+            array_unshift($parent_name_arr,$name);
+        }
+        return implode(' - ',$parent_name_arr);
+    }
 }
