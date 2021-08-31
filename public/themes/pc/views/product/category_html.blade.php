8eff61ee142997d70e5e55c11b12454c6307dcd1
wzj <wzj@eead9d37-86bc-cd46-9d00-32314a84245c>
2021-08-31 06:39:58 +0000

    git-svn-id: https://39.108.144.9:8443/svn/yiduoli_web@225 eead9d37-86bc-cd46-9d00-32314a84245c

--- public/themes/pc/views/product/category_html.blade.php
+++ public/themes/pc/views/product/category_html.blade.php
@@ -2,7 +2,11 @@
 @foreach($lists as $list_key => $list)
     <ul class="{!! num_to_eng_tab($list_key) !!} clearfix category-tab" style="display: block">
         @foreach($list as $key => $category)
-            <li category_id="{{ $category['id'] }}" type="child" @if($category['active']) class="active" @endif>{{ $category['name'] }}</li>
+            @if($category['id'] == 57)
+                <li category_id="{{ $category['id'] }}" type="child" @if($category['active']) class="active" @endif><a href="{{ setting('gym_website') }}" target="_blank">{{ $category['name'] }}</a></li>
+            @else
+                <li category_id="{{ $category['id'] }}" type="child" @if($category['active']) class="active" @endif>{{ $category['name'] }}</li>
+            @endif
         @endforeach
     </ul>
 @endforeach
