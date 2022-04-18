fcff7a306e6a2727b695b236df59b6288dd51fad
VisualSVN Server <VisualSVN Server@eead9d37-86bc-cd46-9d00-32314a84245c>
2022-04-18 08:22:16 +0000

    Deleted folder '/public/themes'.
    git-svn-id: https://39.108.144.9:8443/svn/yiduoli_web@250 eead9d37-86bc-cd46-9d00-32314a84245c

--- public/themes/pc/widgets/current.blade.php
+++
@@ -1,1 +0,0 @@
-@if($nav) 当前位置：@if($parent_nav) <a href="{{ $parent_nav->url }}">{{ $parent_nav->name }}</a> > @endif {{ $nav->name }} @endif
--- public/themes/pc/widgets/left_nav.blade.php
+++
@@ -1,7 +0,0 @@
-
-    <div class="main-nav-title">标本馆导航</div>
-    <div class="main-nav-list fb-clearfix">
-        @foreach($navs as $key => $item)
-            <div class="main-nav-item {{ active_class($item->active) }}"><a href="{{ $item->url }}">{{ $item->name }}</a></div>
-        @endforeach
-    </div>
