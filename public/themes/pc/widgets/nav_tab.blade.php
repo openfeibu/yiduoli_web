f64e903cfb215acb01317383d1abc312fa40489d
wzj <wzj@eead9d37-86bc-cd46-9d00-32314a84245c>
2022-04-18 07:42:54 +0000

    git-svn-id: https://39.108.144.9:8443/svn/yiduoli_web@232 eead9d37-86bc-cd46-9d00-32314a84245c

--- public/themes/pc/widgets/nav_tab.blade.php
+++ public/themes/pc/widgets/nav_tab.blade.php
@@ -1,5 +1,4 @@
-<div class="page-title clearfix wow fadeInUp animated" data-wow-duration=".6s" data-wow-delay=".4s">
-
+<div class="page-title clearfix wow fadeInLeft animated" data-wow-duration=".6s" data-wow-delay=".4s">
     @if(isset($nav))
         <div class="page-title-c pull-left">
             <span>{{ $nav->name }}</span>
@@ -8,9 +7,4 @@
             <span>/</span>{{ $nav->en_name }}
         </div>
     @endif
-    <div class="tab pull-right">
-        @foreach($nav_tabs as $key => $nav_item)
-            <div class="tab-item pull-left @if($nav_item->id == $nav->id)active @endif"><a href="{{ $nav_item->url }}">{{ $nav_item->name }}</a></div>
-        @endforeach
-    </div>
 </div>
